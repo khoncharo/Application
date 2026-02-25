5ed7b725795b459623240f0677d1bc82765a81e9
Your Name <you@example.com>
2026-02-25 18:44:20 +0000

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { EventsModule } from './events/events.module';
+
+@Module({
+  imports: [UsersModule, AuthModule, EventsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
