af64180358e172801df9756a0c35686d4aae9233
Your Name <you@example.com>
2026-03-13 14:25:48 +0000

    feat: db config update

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,24 +1,7 @@
-import { IDatabaseConfig } from './datavase-config.intereface';
-import { IsNumber, IsString } from 'class-validator';
-import { Type } from 'class-transformer';
+import { IDatabaseConfig } from './database-config.intereface';
+import { IsString } from 'class-validator';
 
 export class DatabaseConfig implements IDatabaseConfig {
-  @IsString()
-  host: string;
-
-  @IsNumber()
-  @Type(() => Number)
-  port: number;
-
-  @IsString()
-  username: string;
-
-  @IsString()
-  password: string;
-
-  @IsString()
-  database: string;
-
   @IsString()
   URL: string;
 }
